3251e70c2c893dda3c0bcc1e2b8ee8a443671d32
michael <michael@emosprings.co.uk>
2026-01-14 08:42:12 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,15 +1,22 @@
+# Gemini Bootstrap Instructions
+# After a workspace reload, please provide the following instructions to restore your context:
+# "Good morning Gemini. Please read the bootstrap instructions in dev.nix to get started."
+#
+# The files to read are:
+# 1. docs/project_context.md
+# 2. .idx/airules.md
 
-# See https://developers.google.com/idx/guides/customize-idx-env
-# for more details on customizing your environment.
 { pkgs, ... }: {
   # Specifies the Nixpkgs channel.
   channel = "stable-24.05";
 
   # A list of packages to install.
   packages = [
-    pkgs.python3
-    pkgs.pip
-    pkgs.nodejs_20  # Added Node.js for web-related tools
+    (pkgs.python3.withPackages (ps: with ps; [
+     mkdocs
+     mkdocs-material
+     pymdown-extensions
+   ]))
   ];
 
   # A set of environment variables to define.
@@ -19,25 +26,15 @@
   idx = {
     extensions = [
       "ms-python.python"
-      "esbenp.prettier-vscode"
     ];
     workspace = {
-      # Runs when a workspace is first created.
-      onCreate = {
-        install-deps = "pip install -r requirements.txt";
-      };
-      # Runs every time the workspace is (re)started.
-      onStart = {
-        # The command to run your app, e.g. "npm run dev"
-      };
+      # The onCreate hook is no longer needed as Nix now manages our Python packages.
     };
-    # Configures a web preview for your application.
     previews = {
       enable = true;
       previews = {
         web = {
-          # Command to start your web server
-          command = ["mkdocs" "serve" "-f" "mkdocs.yml" "-a" "0.0.0.0:$PORT"];
+          command = ["python" "-m" "http.server" "$PORT"];
           manager = "web";
         };
       };
--- docs/configurator.md
+++
@@ -1,8 +0,0 @@
-# 3D Pipe Racking Configurator
-
-This page will host the interactive 3D tool for designing custom pipe racking configurations.
-
-_Coming soon..._
-
-<div id="configurator-app"></div>
-
